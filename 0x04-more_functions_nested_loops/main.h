6959b20571de01b3afdd72be823a32bb2ffdda17
Eunice js <Adediraneunice20@gmail.com>
2022-09-16 06:52:45 -0700

    alx

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,3 @@
-#ifndef _main_h
-#define _main_h
-
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -8,11 +5,8 @@ int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
-void print_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
-
 void print_triangle(int size);
-
-#endif
+void print_number(int n);
