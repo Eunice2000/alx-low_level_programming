81a9f4f1e16a225f195004386a371b5faf810d1a
Eunice js <Adediraneunice20@gmail.com>
2022-10-25 08:50:18 -0700

    linked

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,19 +1,21 @@
 #include "lists.h"
+#include <stdlib.h>
+
 /**
- * free_listint - frees the list
- * @head: head of struct
- *
+ * free_listint - frees a listint_t list
+ * solocode
+ * @head: the head of the list
+ * Return: NULL if error
  */
+
 void free_listint(listint_t *head)
 {
-	listint_t *tmp;
-
+	listint_t *next;
 
 	while (head != NULL)
 	{
-		tmp = head;
+		next = head->next;
 		free(head);
-		head = tmp->next;
+		head = next;
 	}
-
 }
